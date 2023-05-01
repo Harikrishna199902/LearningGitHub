2057faf9ff3ad46a8e112373a843b3cef806c521
Harikrishna199902 <132252228+Harikrishna199902@users.noreply.github.com>
2023-05-01 13:50:23 +0530

    Add files via upload

---
+++ reversenum.py
@@ -0,0 +1,7 @@
+number = input("Enter a number: ")
+reversed_number = ""
+
+for i in range(len(number) - 1, -1, -1):
+    reversed_number += number[i] 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+print("Reversed number:", reversed_number)
